5e127353b212a08ae742c84fcd1fddd61dffaed3
Rishma_SK <rishma88@gmail.com>
2020-05-01 00:40:13 +0200

    Updates

---
+++ precache-manifest.384785b5bd59780f8e9e378aa4bf06f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e110dbb5db4b8b892811c616cdf41dd",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "65f0b543c9bef73ddc98",
+    "url": "/react-deploy/static/css/main.3a87de5d.chunk.css"
+  },
+  {
+    "revision": "7e004d8230ae88d96cca",
+    "url": "/react-deploy/static/js/2.9c3c38d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.9c3c38d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f0b543c9bef73ddc98",
+    "url": "/react-deploy/static/js/main.0ad92751.chunk.js"
+  },
+  {
+    "revision": "8e24c887998ac2032a08",
+    "url": "/react-deploy/static/js/runtime-main.ea9f993c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
